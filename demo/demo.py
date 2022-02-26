5ee611d33229950a136dbd978672678e1236389f
Markus Hirsimäki <hirsimaki.markus@gmail.com>
2022-02-26 20:04:17 +0200

    librarifying

---
+++ demo/demo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+This file is provided as a demonstration tool for using pystributor
+"""
+
+from os import system
+
+
+def main():
+    print("This python file is provided as a demo which uses pystributor to")
+    print("perform an example task. Different systems will have different")
+    print("performance characteristics. This will affect much performance")
+    print("can be gained by using pystributor. Certain tasks will perform")
+    print("worse with pystributor due to increased overhead.")
+    print()
+    print("If you test your own tasks, the most benefit will likely be")
+    print("gained for tasks that have a moderate amount of argument sets to")
+    print("distribute (in order of 100s or 1000s). Each argument should")
+    print("be maximally CPU intensive when processed on worker.")
+
+
+
+
+
+
+if __name__ == "__main__":
+    _ = system("cls||clear") # clear screen on windows and unix
+    main()
+
+
+
+
+
+# TÖYT TÄHÄN ASTI: 18H PER NASSU
+# ja makelle ehkä 3h lisää
---
+++ pystributor/pystributor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+
+from os import system
+
+from pystributor_hub import *
+
+
+
+
+
+def main():
+    print("You should not be running this file. See provided demo for usage")
+
+if __name__ == "__main__":
+    _ = system("cls||clear") # clear screen on windows and unix
+    main()
+
+
+hub = None
